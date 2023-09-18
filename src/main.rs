c1435885d55ec244b9a58f87d815365f03bdb824
eric-jacobson <ecjacobson9@gmail.com>
2023-09-18 03:24:13 -0600

    reorganize project

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,3 @@
-mod day_1;
-mod day_2;
-mod day_3;
-
 fn main() {
-    println!("********** Day 1 **********");
-    day_1::counting_calories();
-
-    println!("\n********** Day 2 **********");
-    day_2::rock_paper_scissors();
-
-    println!("\n********** Day 3 **********");
-    day_3::rucksack_reorganization();
+    println!("Run with cargo run --bin <day>");
 }

a9fabe4a995feafc0ed38338319fac5c0b10fa46
eric-jacobson <ecjacobson9@gmail.com>
2023-09-28 17:54:21 -0600

    day 5 part 2

--- src/bin/day_5.rs
+++ src/bin/day_5.rs
@@ -4,17 +4,14 @@ use itertools::Itertools;
 
 #[derive(Debug)]
 struct Move {
+    count: usize,
     from: usize,
     to: usize,
-    count: usize,
 }
 
 fn main() {
     let input = fs::read_to_string("inputs/supply-stacks").expect("failed to read the file");
     let data: (&str, &str) = input.split("\n\n").collect_tuple().unwrap();
-
-    let mut stacks = build_stacks(data.0.lines().collect_vec());
-
     let moves: Vec<Move> = data
         .1
         .lines()
@@ -26,17 +23,28 @@ fn main() {
         })
         .collect();
 
-    for item in moves {
+    let mut stacks = build_stacks(data.0.lines().collect_vec());
+    for item in &moves {
         for _ in 0..item.count {
             let item_to_move = stacks[item.from - 1].pop_front().unwrap();
             stacks[item.to - 1].push_front(item_to_move);
         }
     }
-
     let part_one = get_top_of_stacks(stacks);
-
     println!("Part 1: {:?}", part_one);
-    println!("Part 2: {:?}", "TODO");
+
+    let mut stacks = build_stacks(data.0.lines().collect_vec());
+    for item in &moves {
+        let mut items_to_move = VecDeque::new();
+        for _ in 0..item.count {
+            items_to_move.push_front(stacks[item.from - 1].pop_front().unwrap());
+        }
+        for c in items_to_move {
+            stacks[item.to - 1].push_front(c);
+        }
+    }
+    let part_two = get_top_of_stacks(stacks);
+    println!("Part 2: {:?}", part_two);
 }
 
 fn get_top_of_stacks(stacks: Vec<VecDeque<char>>) -> String {

739d88aa320ac13a54f65ee0c98a75a244869b5e
eric-jacobson <ecjacobson9@gmail.com>
2024-12-04 21:45:17 -0700

    2024 day 2

---
+++ 2024/cmd/day_02/main.go
@@ -0,0 +1,68 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	input, err := os.ReadFile("cmd/day_02/input")
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println("Part 1, totalSafeReports: ", solve(input, false))
+	fmt.Println("Part 2, totalSafeReportsDampened: ", solve(input, true))
+}
+
+func solve(input []byte, dampen bool) int {
+	totalSafeReports := 0
+
+	for _, line := range strings.Split(strings.TrimRight(string(input), "\n"), "\n") {
+		values := strings.Fields(line)
+		if validateSafety(values) { // part 1
+			totalSafeReports++
+		} else {
+			if dampen { // part 2
+				for i := 0; i < len(values); i++ {
+					dampenedValues := append([]string{}, values[:i]...)
+					dampenedValues = append(dampenedValues, values[i+1:]...)
+					if validateSafety(dampenedValues) {
+						totalSafeReports++
+						break
+					}
+				}
+			}
+		}
+	}
+	return totalSafeReports
+}
+
+func validateSafety(values []string) bool {
+	increasing, decreasing := false, false
+	for i := 1; i < len(values); i++ {
+		current, _ := strconv.Atoi(values[i])
+		previous, _ := strconv.Atoi(values[i-1])
+
+		delta := absouluteValue(current - previous)
+		if (current > previous && decreasing) || (current < previous && increasing) || current == previous || !(delta >= 1 && delta <= 3) {
+			return false
+		}
+
+		if current > previous {
+			increasing = true
+		} else if current < previous {
+			decreasing = true
+		}
+	}
+	return true
+}
+
+func absouluteValue(i int) int {
+	if i < 0 {
+		return -i
+	}
+	return i
+}
